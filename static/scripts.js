088f008acc0dad632b113ad625dc18fd1d0fa02d
Tasos Bachtses <56441494+bachtses@users.noreply.github.com>
2025-09-29 16:10:56 +0300

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -983,7 +983,7 @@ function showPage(pageId) {
 //////////////////////////////////////////////////////////////////////////////////////////////////
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/static/service-worker.js")
+    navigator.serviceWorker.register("/service-worker.js")
       .then(reg => console.log("Service Worker registered:", reg))
       .catch(err => console.error("Service Worker registration failed:", err));
   });
